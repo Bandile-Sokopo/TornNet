56d899df500a85f11b510b2393b73a9849fafa24
Bandile <133995097+Bandile-Sokopo@users.noreply.github.com>
2025-06-02 08:04:56 +0200

    -types created and used for appcomponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { item } from './shared/Models/item';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,11 @@ import { HeaderComponent } from "./layout/header/header.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'TornNet';
+baseUrl = "http://localhost:5282/api";
+private http = inject(HttpClient);
+
+title = 'TornNet';
+
+items: item[] = [];
+
 }
---
+++ client/src/app/shared/Models/item.ts
@@ -0,0 +1,13 @@
+export type item = {
+    name: string;
+    description: string;
+    effect: string;
+    requirement: string;
+    type: string;
+    weaponType: string;
+    buyPrice: number;
+    sellPrice: number;
+    MarketValue: number;
+    circulation: number;
+    image: string;
+}
